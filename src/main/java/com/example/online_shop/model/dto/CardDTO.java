af9dc54bf6e01d8a2c71afa4c37de16c213ee215
Антон <toshabespalov2006@gmail.com>
2023-06-24 22:22:28 +0300

    patch 0.1.0

--- src/main/java/com/example/online_shop/model/dto/CardDTO.java
+++ src/main/java/com/example/online_shop/model/dto/CardDTO.java
@@ -3,5 +3,5 @@ package com.example.online_shop.model.dto;
 import java.sql.Timestamp;
 import java.util.List;
 
-public record CardDTO(String name, String cardNumber, String cvv, Timestamp cardValidity, List<UserDTO> owner) {
+public record CardDTO(String name, String cardNumber, String cvv, Timestamp cardValidity, List<UserDTO> owners) {
 }
